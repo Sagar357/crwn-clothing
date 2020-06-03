565c566555e888782b6fe51ded7926c51f8772f0
markushno357@gmail.com <markushno357@gmail.com>
2020-06-03 16:59:15 +0530

    add router withRouter to menu-item component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './App.css';
 import HomePage from './Pages/homepage/Homepage.component';
+import { Switch ,Route } from 'react-router-dom';
+
+const hatspage=()=>(
+  <div>
+    <h1>
+      HATS
+    </h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+      <Route  exact path='/' component={HomePage} />
+      <Route  exact path='/hats' component={hatspage} />
+      </Switch>
     </div>
   );
 }
--- src/Components/menu-item/menu-item.component.jsx
+++ src/Components/menu-item/menu-item.component.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import '../menu-item/menu-item.styles.scss'
+import '../menu-item/menu-item.styles.scss';
+import {withRouter} from 'react-router-dom';
 
 const MenuItem =(props)=> {
     return  ( <div 
      className={`${props.size} menu-items`}>
     <div className='background-image'
+    onClick={() =>
+        {
+            props.history.push(`${props.match.url}${props.linkUrl}`);
+        }}
      style=
      {{
         backgroundImage:`url(${props.url})`,
@@ -18,4 +23,4 @@ const MenuItem =(props)=> {
     );
 }
 
-export default MenuItem;
+export default withRouter(MenuItem);
